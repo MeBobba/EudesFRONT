70790669eb2e354cec797f67d8c82b2ae18b494a
Swayn <36993464+SwaynIO@users.noreply.github.com>
2024-07-26 20:09:26 +0200

    Update AppStaff.vue

--- src/views/AppStaff.vue
+++ src/views/AppStaff.vue
@@ -24,7 +24,7 @@
                                         </div>
                                         <button v-if="canEditStaff" @click="openEditModal(user)"
                                             class="absolute top-0 right-0 bg-blue-500 text-white rounded-full p-2 z-20">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
                                                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M15.232 5.232l3.536 3.536m-2.036-4.036a2.5 2.5 0 113.536 3.536L6.5 18.5H3v-3.5L16.732 3.732z" />
